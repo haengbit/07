e39040ac53cca55cee5e594340381ebbcba43e84
haengbit <110611005+haengbit@users.noreply.github.com>
2022-10-18 17:07:37 +0900

    7주차_실습 6-1

--- main.c
+++ main.c
@@ -3,26 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int arc, char *argv[])
-{
-	int i;
-	for (i = 0; i < 3; i++)
-		sub();
-	return 0;
-}
+int all_files;
 
-void sub(void)
-{
-	int auto_count = 0; 
-	static int static_count = 0; 
-	
-	auto_count++;
-	static_count++;
-	
-	printf("auto_count=%d\n", auto_count);
-	printf("static_count=%d\n", static_count);
-}
+extern void sub();
 
+int main(void)
+{
+	sub();
+	printf("%d\n", all_files);
+	return 0;	
+} 
 
 
 
