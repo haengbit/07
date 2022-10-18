948a86ba609d7822f67d893436434f335a5d9ac9
haengbit <110611005+haengbit@users.noreply.github.com>
2022-10-18 16:47:44 +0900

    7주차_실습 4

--- main.c
+++ main.c
@@ -3,23 +3,24 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+void f(void);
 
-int main(int argc, char *argv[]) 
+int i;
+int main(void)
 {
-	int i = 10;
-	printf("before calling function i=%d\n", i);
-	inc(i);
-	printf("after calling function i=%d\n", i);
-	return 0; 
+	for (i = 0; i < 5; i++)
+	{
+		f();
+	}
+	return 0;
 }
 
-int inc(int counter)
+void f(void)
 {
-	counter++;
-	return counter;
+	for (i = 0; i < 10; i++)
+		printf("#");
 }
 
-// before calling function i=10
-// after calling function i=10
-
+// result
+// ##########
 
